606c058f7a5c7710bac7aa1e54be244dd5f50e8f
Manish Raj <manish_2312res370@iitp.ac.in>
2026-01-16 22:26:58 +0530

    sixth commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // Ensure this is imported
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+import "./assets/styles.css";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    {/* ADD THE BASENAME HERE 👇 */}
-    <BrowserRouter basename="/v1profile">
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
