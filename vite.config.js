7c97a508538c00fb9adc5378be8316b2042f492d
Manish Raj <manish_2312res370@iitp.ac.in>
2026-01-21 23:01:50 +0530

    fourteenth commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/v1profile/", // <--- ADD THIS (Replace with your actual repo name)
+  base: "/digitalportfolio/", // <--- ADD THIS (Replace with your actual repo name)
 });
