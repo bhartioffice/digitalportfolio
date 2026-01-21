1516b1ae98b90009fd0e991adf51c35a1b048f16
Manish Raj <manish_2312res370@iitp.ac.in>
2026-01-21 23:49:35 +0530

    ready for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/digitalportfolio/", // <--- ADD THIS (Replace with your actual repo name)
+export default defineConfig(({ mode }) => {
+  const isNetlify = process.env.NETLIFY === "true";
+
+  return {
+    plugins: [react()],
+    base: isNetlify ? "/" : mode === "production" ? "/digitalportfolio/" : "/",
+  };
 });
